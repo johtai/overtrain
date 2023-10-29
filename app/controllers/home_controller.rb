87dd96cf290c6548925b269cc7168257010a50e5
Tempire <danil.varlamov.03@mail.ru>
2023-10-29 18:52:16 +0300

    implement show_all.html.erb, show.html.erb &&  refactor  home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,21 +4,47 @@ class HomeController < ApplicationController
   end
   def error
   end
-  def show_all
-    usernames = helpers.get_all_usernames(params[:username])
 
-    if usernames["total"] == 1
-      show
-    end
-
-    @response = usernames
-  end
   def show
     username = helpers.get_username(params[:username])
 
-    if username.nil?
-      redirect_to "/error"
+    if !username.nil? & !username['error']
+
+      return @response = username
     end
-    @response = username
+
+
+    if username['error'] & username.blank?
+      if helpers.get_all_usernames(params[:username])["total"] == 0
+        redirect_to error_path
+      end
+
+
+      if helpers.get_all_usernames(params[:username])["total"] == 1
+        rsp = helpers.get_all_usernames(params[:username])
+        username = rsp['results'][0]['player_id']
+        return @response = helpers.get_username(username)
+
+      end
+
+    end
+
+
+    if(helpers.get_all_usernames(params[:username])["total"] > 1)
+
+      redirect_to show_all_path(username: params[:username])
+    end
+
+
+    #@response = helpers.get_username(username)
+  end
+
+
+
+  def show_all
+    usernames = helpers.get_all_usernames(params[:username])
+    uname = params[:username]
+
+    @response = usernames
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  post  "/get_username", to: "home#show_all"
-  get   "/get_username", to: "home#show_all"
+  post  "/get_username", to: "home#show"
+  get   "/get_username", to: "home#show"
 
   get "/error", to: "home#error"
   root "home#index"
   #rails routes -g session
   resources :users, only: %i[new create]
   resource :session, only: %i[new create destroy]
+  get '/show_all', to: 'home#show_all'
 
 end
