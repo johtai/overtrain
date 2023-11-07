98590d3de21f665d459532bd01a648bd7b0b2d60
ompus <babincevva@ya.ru>
2023-11-07 09:21:01 +0300

    add validations for player and user

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,5 @@
 class Player < ApplicationRecord
   has_many :subscriptions
   has_many :players, :through => :subscriptions
+  validates :kda, :winrate,:avg_elim, :avg_ass, :avg_death, :avg_dam, :avg_heal, numericality: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :subscriptions
   has_many :users, :through => subscriptions
+  validates :nickname, uniqueness: true
   has_secure_password
 end
