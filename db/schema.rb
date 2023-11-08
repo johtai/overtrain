5a6849d4379aa04539089d535ca538d11111eadf
johtai <peterwaybright@gmail.com>
2023-11-08 16:34:03 +0300

    add migration: rename table Heros to Heroes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_08_113333) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_08_133218) do
   create_table "gamemodes", force: :cascade do |t|
     t.string "key"
     t.string "name"
@@ -21,7 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_08_113333) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "heros", force: :cascade do |t|
+  create_table "heroes", force: :cascade do |t|
     t.string "hero_id"
     t.string "name"
     t.string "desc"
