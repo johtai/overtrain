12f26ec2d0f2d2879587a899cbc3cf4e02185642
johtai <peterwaybright@gmail.com>
2023-11-08 14:52:44 +0300

    add TODO lines to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,23 @@ Rails.application.routes.draw do
   get "/search" => "home#search"
   get "/username/:username" => "home#show"
   get "/error" => "home#error"
-  
+
+  # TODO
+  # get /profile/:user_id
+  # get /profile/:user_id/edit
+  # patch /profile/:user_id/edit
+  # get /profile/:user_id/subscriptions
+  # delete /profile/:user_id/subscriptions
+  # post /profile/:used_id/subscription
+  #
+  # get "/wiki"
+  # get "/wiki/heroes"
+  # get "/wiki/heroes/:id"
+  # get "/wiki/gamemodes"
+  # get "/wiki/gamemodes/:id"
+  # get "/wiki/maps"
+  # get "/wiki/maps/:id"
+
   root "home#index"
 
   resources :users, only: %i[new create]
