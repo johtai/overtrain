897aee8ebe7114824e895129b13caa7909a42535
johtai <peterwaybright@gmail.com>
2023-11-23 22:07:38 +0300

    add helpers for creating Map & Gamemode wiki databases

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,43 @@ module ApplicationHelper
     unparsed_response = Net::HTTP.get_response(uri)
     JSON.parse(unparsed_response.body)
   end
+  def create_maps_db
+    uri = URI("https://overfast-api.tekrop.fr/maps")
+    unparsed_response = Net::HTTP.get_response(uri)
+    maps = JSON.parse(unparsed_response.body)
+
+    maps.each do |map|
+      new_map = Map.create(name: map["name"], screenshot: map["screenshot"], gamemodes: map["gamemodes"],
+                           location: map["location"], country_code: map["country_code"])
+      new_map.save()
+    end
+  end
+  def create_gm_db
+    uri = URI("https://overfast-api.tekrop.fr/gamemodes")
+    unparsed_response = Net::HTTP.get_response(uri)
+    gms = JSON.parse(unparsed_response.body)
+
+    gms.each do |gm|
+      new_gm = Gamemode.create(key: gm["key"], name: gm["name"], icon: gm["icon"],
+                               desc: gm["description"], screenshot: gm["screenshot"])
+      new_gm.save()
+    end
+  end
+  def create_heroes_db
+    uri = URI("https://overfast-api.tekrop.fr/heroes")
+    unparsed_response = Net::HTTP.get_response(uri)
+    heroes = JSON.parse(unparsed_response.body)
+
+    heroes.each do |hero|
+      uri = URI("https://overfast-api.tekrop.fr/heroes/#{hero["key"]}")
+      unparsed_response = Net::HTTP.get_response(uri)
+      attributes = JSON.parse(unparsed_response.body)
+
+      new_hero = Hero.create(key: hero["key"], name: attributes["name"], description: attributes["description"],
+                               portrait: attributes["portrait"], role: attributes["role"],
+                               location: attributes["location"], hitpoints: attributes["hitpoints"],
+                               abilities: attributes["abilities"], story: attributes["story"])
+      new_hero.save()
+    end
+  end
 end
