dcc0493bdaa6941a4a83164eb3ab95583ba48406
Zubarev Daniil <dzubarev@sfedu.ru>
2023-12-12 00:40:39 +0300

    add tests

--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class PlayerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save player without id" do
+    Player = player_id.new
+    assert_not Player.save, "Saved the player without id"
+  end
 end
